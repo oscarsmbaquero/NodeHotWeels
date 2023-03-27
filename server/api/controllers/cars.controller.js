8fc499904ae011be9237423723d46ded6c9eb652
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-03-27 12:07:50 +0200

    create ok

--- server/api/controllers/cars.controller.js
+++ server/api/controllers/cars.controller.js
@@ -1,16 +1,15 @@
-
 import { httpStatusCode } from "../../utils/httpStatusCode.js";
 import { Cars } from "../models/Cars.Model.js";
-import { ObjectId } from 'mongodb';
+import { ObjectId } from "mongodb";
 
 const getCars = async (req, res, next) => {
- console.log('Entro')
+  console.log("Entro");
   try {
-    const cars = await Cars.find()
-      // .populate({ path: "materialIntervencion",select: "descripcion"})
-      // .populate({path:'cliente', select :""})
-      console.log(cars,12)
-      return res.status(200).json(cars);
+    const cars = await Cars.find();
+    // .populate({ path: "materialIntervencion",select: "descripcion"})
+    // .populate({path:'cliente', select :""})
+    console.log(cars, 12);
+    return res.status(200).json(cars);
     // return res.json({
     //   //  status : 200,
     //   //  message : httpStatusCode[200],
@@ -25,11 +24,11 @@ const getCars = async (req, res, next) => {
 const carDetail = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const cars = await Cars.findById(id)
-      // .populate({ path: "materialIntervencion",select: "descripcion"})
-      // .populate({path:'cliente', select :''})
+    const cars = await Cars.findById(id);
+    // .populate({ path: "materialIntervencion",select: "descripcion"})
+    // .populate({path:'cliente', select :''})
     return res.status(200).json(cars);
- 
+
     return res.json({
       //  status : 200,
       //  message : httpStatusCode[200],
@@ -41,40 +40,24 @@ const carDetail = async (req, res, next) => {
   }
 };
 
-
 const createCars = async (req, res, next) => {
+  console.log("Entro");
+  console.log(req.body, 46);
   try {
-    const NewCar = new Avisos({
-      marca: req.body.averia,
-      modelo: req.body.prioridad,
-      imagen: req.body.estado,
+    const NewCar = new Cars({
+      marca: req.body.marca,
+      modelo: req.body.modelo,
+      imagen: req.body.imagen,
     });
     const newCarDB = await NewCar.save();
-    //const idCliente = newAvisoDB._id;
-    await Cars.updateOne(
-      { _id: newCarDB.cliente },
-      { $push: { cars: newCarDB._id } },
-      { new: true }
-    );
-    // await Clientes.updateOne(
-    //   { _id: newAvisoDB.cliente },
-    //   { $push: { averia: averia } },
-    //   { new: true }
-    // );
-
-
     return res.json({
       status: 201,
       message: httpStatusCode[201],
-      data: { car: newCarDB },
+      data: { cars: newCarDB },
     });
   } catch (error) {
     return next(error);
   }
 };
 
-export {
-  getCars,
-  carDetail,
-  createCars,
-};
+export { getCars, carDetail, createCars };
